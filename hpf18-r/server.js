45d509e0da5a58fa9ce63e2aa05b8a07ea002cf6
Robert Juall <RJuall@gmail.com>
2018-11-10 13:34:50 -0500

    Setting up the React frontend

--- hpf18-r/server.js
+++ hpf18-r/server.js
@@ -20,6 +20,6 @@ mongoose
 // Use Routes
 app.use('/api/items', items);
 
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 
 app.listen(port, () => console.log(`Server started on ${port}`));
